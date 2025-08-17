919c8b9291bec56069968f34e88374d8d5d687e1
Ahmed Elsadek <sadek@innovix.net>
2025-08-17 14:31:18 +0300

    feat: create static product details page.

--- RedBubble/src/app/app.config.ts
+++ RedBubble/src/app/app.config.ts
@@ -1,12 +1,14 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter, withComponentInputBinding } from '@angular/router';
+import { ApplicationConfig } from '@angular/core';
+import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
+import { provideReactiveForms } from '@angular/forms';
+
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes, withComponentInputBinding()),
-    provideHttpClient()
+    provideRouter(routes),
+    provideHttpClient(),
+    provideReactiveForms(),
   ]
 };
